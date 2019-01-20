43ba833b4b88ba9d478e1092a54667b08cc350cf
David Hill <david.hill@bison.howard.edu>
2019-01-20 16:51:35 -0500

    Initializing all game values

--- Dice-Game/app.js
+++ Dice-Game/app.js
@@ -18,6 +18,13 @@ activeplayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+
 document.querySelector('.btn-roll').addEventListener('click', roll);
 
 function roll(){
