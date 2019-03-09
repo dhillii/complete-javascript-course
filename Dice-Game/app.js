1e9fb1b628bd740a201cc92d2234af6b2a0e9bdd
David Hill <david.hill@bison.howard.edu>
2019-03-09 18:01:25 -0500

    Working

--- Dice-Game/app.js
+++ Dice-Game/app.js
@@ -13,6 +13,8 @@ var playerScores, roundScore, activePlayer;
 
 init();
 
+
+
 function init(){
     playerScores = [0,0];
     roundScore = 0;
@@ -24,10 +26,11 @@ function init(){
     document.getElementById('current-1').textContent = '0';
     document.getElementById('name-0').textContent = 'Player 1';
     document.getElementById('name-1').textContent = 'Player 2';
-    document.getElementById('.player-0-panel').classList.remove('winner');
-    document.getElementById('.player-1-panel').classList.remove('winner');
-    document.getElementById('.player-0-panel').classList.remove('active');
-    document.getElementById('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
 }
 
 document.querySelector('.btn-roll').addEventListener('click', roll);
